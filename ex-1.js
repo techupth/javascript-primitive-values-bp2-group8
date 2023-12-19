26ae774ae34a23149375d34c3d1d9977f6b104c6
Krittin <rubruengyod@gmail.com>
2023-12-19 10:15:24 +0700

    feat:Primitives Assignment Completed

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,6 @@
 // Exercise #1: Undefined ให้ใส่ Comment ด้วยข้อความว่า undefine ต่อท้ายในบรรทัดที่มี Value เป็น undefined
 let day = 30;
-let employeeName; //undefined
-let productName = undefined; //undefined
-let order; //undefined
+let employeeName; // undefined
+let productName = undefined; // undefined
+let order; // undefined
 
